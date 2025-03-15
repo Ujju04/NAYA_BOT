e6be529bd2e20cdbe74c055479ac1be38a65a038
ujjwal kumar <119151521+Ujju04@users.noreply.github.com>
2025-03-15 14:39:45 +0000

    crypto

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import crypto from 'crypto-browserify';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Define global.crypto to use the crypto polyfill
+    'global.crypto': crypto,
+  },
 })
